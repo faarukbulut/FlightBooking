967bb9b3cc0a845a9226d805ca45b53547c9998f
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2026-05-15 19:50:04 +0300

    Machine Learning Modelini Eğitme ve Test Etme

--- Areas/Admin/Controllers/ForecastController.cs
+++ Areas/Admin/Controllers/ForecastController.cs
@@ -1,4 +1,5 @@
-﻿using FlightBooking.MLServices;
+﻿using FlightBooking.MLModels;
+using FlightBooking.MLServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlightBooking.Areas.Admin.Controllers
@@ -21,6 +22,28 @@ namespace FlightBooking.Areas.Admin.Controllers
             _flightMlService.Train(mlData);
 
             ViewBag.Message = "Model başarıyla eğitildi.";
+            return View();
+        }
+
+        public IActionResult Predict()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Predict(DateTime flightDate, string flightType)
+        {
+            var input = new FlightData
+            {
+                Month = flightDate.Month,
+                DayOfWeek = (float)flightDate.DayOfWeek,
+                FlightType = flightType == "Morning" ? 0 : 1
+            };
+
+            var prediction = _flightMlService.Predict(input);
+            ViewBag.Result = prediction.PredictedLabel ? "Bu uçuş büyük ihtimalle dolacaktır." : "Bu uçuşta yoğunluk düşük görünüyor.";
+
+            ViewBag.Probability = prediction.Probability;
             return View();
         }
     }
