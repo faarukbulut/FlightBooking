7e98f058316c376ba7064d4233a08d1e84b62704
Faruk Bulut <72122797+faarukbulut@users.noreply.github.com>
2026-05-15 16:00:01 +0300

    Create Booking & Passenger Sayfası

---
+++ Areas/Admin/Controllers/BookingsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BookingsController : Controller
+    {
+        [HttpGet]
+        public IActionResult CreateBooking()
+        {
+            return View();
+        }
+    }
+}
